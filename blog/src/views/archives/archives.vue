88cd8c88ad4a4bb4e3255b880bd0099f29ce86cd
shcwenxin <shcwenxin>
2020-12-16 23:29:31 +0800

    上传代码

--- blog/src/views/archives/archives.vue
+++ blog/src/views/archives/archives.vue
@@ -1,7 +1,144 @@
 <template>
   <div class="archives">
     <div class="container">
-      <h1>奥义-时光倒流之术</h1>
+      <div class="archivesHeader flex">
+        <h1>时间线</h1>
+        <i class="iconfont icon-Clock"></i>
+        <h1>奥义-时光倒流之术</h1>
+      </div>
+      <div class="archivesBody">
+        <div class="timeline-line"></div>
+        <div class="year">
+          <h1>2020</h1>
+          <ul>
+            <li class="flex">
+              <h2 style="text-align:right;width:32.8%">2020.01.03</h2>
+              <i class="iconfont icon-center"></i>
+              <div style="width:50%">
+                <p>165315135165153135</p>
+                <p>165315135165153135</p>
+              </div>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+            <li class="flex">
+              <h2>2020.01.03</h2>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
