a4a7e5872cf135b273c075c9c7abf61a802950df
Vixcity <2091283625@qq.com>
2021-07-04 23:07:01 +0800

    还原contentBanner的图片显示效果

--- blog/src/components/contentBanner/contentBanner.vue
+++ blog/src/components/contentBanner/contentBanner.vue
@@ -3,8 +3,7 @@
     --绑定 imgSrc
     --    title
   -->
-  <div class="contentBanner">
-    <img :src="imgSrc" alt="">
+  <div class="contentBanner" :style="styles">
     <div class="page-site-info">
       <h1 id="site-title">{{ title }}</h1>
     </div>
@@ -15,6 +14,31 @@
 export default {
   name: "contentBanner",
   props:['imgSrc','title'],
+  mounted() {
+    this.styles['background-image'] = 'url(' + this.imgSrc || this.styles['background-image'] + ')'
+  },
+  data(){
+    return{
+      styles:{
+        "background-image":"url('https://cdn.jsdelivr.net/gh/jerryc127/butterfly_cdn@2.1.0/top_img/archive.jpg')",
+        "position": "relative",
+        "width": "100%",
+        "background-color": "#49b1f5",
+        "background-position": "center center",
+        "background-size": "cover",
+        "background-repeat": "no-repeat",
+        "transition": "all .5s"
+      }
+    }
+  },
+  watch:{
+    imgSrc(newVal){
+      this.styles['background-image'] = 'url(' + newVal || this.styles['background-image'] + ')'
+    }
+  },
+  methods:{
+
+  }
 }
 </script>
 
